fe976261367c664b017e8c197b5129e599acab7b
Ben Oh <moh16@my.bcit.ca>
2026-06-03 18:52:46 -0700

    add enqueue function that enqueues clean up job

--- jobs/queue.py
+++ jobs/queue.py
@@ -2,6 +2,7 @@ import os
 from rq import Queue
 from redis import Redis
 from jobs.tasks.render import render_video_job
+from jobs.tasks.cleanup import clean_up_expired_jobs
 from jobs.redis_conn import redis_conn
 
 
@@ -18,4 +19,7 @@ def enqueue_render_job(job_id: str, session_id: str, user_id: str, bucket_name: 
             "burned_video_bucket" : burned_video_bucket
         }
     )
+
+def enqueue_clean_up_job():
+    redis_queue.enqueue(clean_up_expired_jobs)
     
