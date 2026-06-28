b8e3636d0af8befd4065f8b836cae61002a7a69c
Ben Oh <moh16@my.bcit.ca>
2026-06-28 10:12:52 -0700

    finish kinetic word caption component

--- renderers/remotion/src/captionStyles/kineticCaption.tsx
+++ renderers/remotion/src/captionStyles/kineticCaption.tsx
@@ -4,9 +4,8 @@ import { useVideoConfig, useCurrentFrame } from "remotion";
 import { getStyleData } from "../utils/styleData";
 
 export function KineticWordCaption({transcript, segmentStyles}: CaptionProp) {
-    const {fps, durationInFrames} = useVideoConfig()
+    const {fps} = useVideoConfig()
     const frame = useCurrentFrame()
-    const videoLength = durationInFrames / fps 
     const videoCurrentTime = frame / fps 
     const currentSegment = transcript?.segments.find((segment) => {
         return segment.start <= videoCurrentTime && segment.end >= videoCurrentTime
@@ -15,9 +14,15 @@ export function KineticWordCaption({transcript, segmentStyles}: CaptionProp) {
 
     return (
         <AbsoluteFill
-        style={styleData}
-        >
-
+            style={styleData}
+        > 
+            {currentSegment?.words.map((wordData) => {
+                if (wordData.start >= videoCurrentTime && wordData.end <= videoCurrentTime) {
+                    return <span className="word active">{wordData.word}</span>
+                } else {
+                    return <span className="word">{wordData.word}</span>
+                }
+            })}
         </AbsoluteFill>
     )
 }
