f0af267fbbbf75d21d0d877ff119e570e8cf3fc1
Ben Oh <moh16@my.bcit.ca>
2026-08-03 12:44:39 -0700

    set modal secret for env vars

--- jobs/modal/transcribe_worker.py
+++ jobs/modal/transcribe_worker.py
@@ -5,14 +5,15 @@ import tempfile
 import boto3
 import subprocess 
 
+load_dotenv()
 
 app = modal.App()
 
-@app.cls(cpu=8)
+@app.cls(cpu=8, secrets=[modal.Secret.from_name("transcript-maker")])
 class TranscriptMaker:
 
     @modal.enter()
-    def startup(self):
+    def startup(self): 
         from faster_whisper import WhisperModel # type: ignore
         self.model = WhisperModel("distil-large-v3", device="cuba")
 
@@ -34,9 +35,9 @@ class TranscriptMaker:
     def __convert_to_audio(self, s3_bucket_info, vid_file, audio_file):
         s3_client = boto3.client(
             "s3",
-            aws_access_key_id=os.getenv("AWS_S3_ACCESS_KEY"),
-            aws_secret_access_key=os.getenv("AWS_S3_SECRET_KEY"),
-            region_name=os.getenv("AWS_REGION"),
+            aws_access_key_id=os.environ("AWS_S3_ACCESS_KEY"),
+            aws_secret_access_key=os.environ("AWS_S3_SECRET_KEY"),
+            region_name=os.environ("AWS_REGION"),
         ) 
         s3_client.download_file(s3_bucket_info.get("bucket"), s3_bucket_info.get("obj_name"), vid_file)
         subprocess.run(
